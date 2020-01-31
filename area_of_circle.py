a47adf58ef75815f1aab80b25de96b80973d9f17
bshafer8 <bshafe8@gmail.com>
2020-01-31 14:49:51 -0500

    Commit of code for the area of a circle

---
+++ area_of_circle.py
@@ -0,0 +1,18 @@
+"""
+Author: Brandon Shafer
+Created: January 21, 2020
+Python Version: 3.7.4
+Time:
+Note: In class exercise for Lecture 3
+"""
+
+
+pi = 3.14  # rounded off
+r = 2.0  # defining the radius
+area = pi * (r**2)  # formula for the area of a circle
+perim = 2.0 * pi * r  # formula for the perimeter of a circle
+
+#These lines output the results
+print("Given r: ", r)
+print("Area is: ", area)
+print("Perimeter is: ", perim)
